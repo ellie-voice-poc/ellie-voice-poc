4c3c7c632eb745f1bd996311effaa6d2be8bd490
ellie-voice-poc <jimbonfield@gmail.com>
2025-08-09 10:23:25 -0700

    Update voice.js

--- api/voice.js
+++ api/voice.js
@@ -1,7 +1,7 @@
 export default async function handler(req, res) {
   const twiml = `<?xml version="1.0" encoding="UTF-8"?>
 <Response>
-  <Say voice="Polly.Joanna">Hi, this is Ellie. Your phone webhook is live.</Say>
+  <Say voice="alice">Hi, this is Ellie. Your phone webhook is live.</Say>
   <Hangup/>
 </Response>`;
   res.setHeader("Content-Type", "application/xml");
